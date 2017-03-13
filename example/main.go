21bacf6e9b40f25eb92668907db824a20e5b06b2
wumingyu02 <317808023@qq.com>
2017-03-13 14:02:24 +0800

    添加写多个线圈

--- example/main.go
+++ example/main.go
@@ -8,9 +8,19 @@ import (
 func main() {
 	pack := &gmodbus.PacketEnCode{}
 	_, b := pack.WriteSingleCoil(0x0f, 1, true)
-	fmt.Println(b)
+	printHex(b)
 
+	//写多个线圈
+	_, b = pack.WriteMultipleCoil(0x0f, 0, 5, []byte{0xff})
+	printHex(b)
 	// if 0x1241 != crc.value() {
 	// 	t.Fatalf("crc expected %v, actual %v", 0x1241, crc.value())
 	// }
+}
+
+func printHex(b []byte) {
+	for _, v := range b {
+		fmt.Printf("%02x ", v)
+	}
+	fmt.Printf("\r\n")
 }
--- packetparse.go
+++ packetparse.go
@@ -44,6 +44,7 @@ func uint16ToByte(in uint16) []byte {
 	return out
 }
 
+//打开单个线圈
 //0F 05 00 00 FF 00 8D 14
 //0F(从机地址) 05（功能码） 00 00（线圈地址） FF 00（开启，关闭为00 00） 8D 14（crc）
 func (self *PacketEnCode) WriteSingleCoil(slaveAddr byte, addr uint16, ifopen bool) (error, []byte) {
@@ -57,6 +58,25 @@ func (self *PacketEnCode) WriteSingleCoil(slaveAddr byte, addr uint16, ifopen bo
 	} else {
 		packet = append(packet, []byte{0x00, 0x00}...)
 	}
+	//加上crc
+	var crc crc
+	crc.reset()
+	crc.pushBytes(packet)
+	packet = append(packet, crc.bytevalue()...)
+	return nil, packet
+}
+
+//打开多个线圈
+//0F 0f 00 00 00 03 01 ff
+//0F(从机地址) 0f（功能码） 00 00（起始地址） 00 03（写线圈个数） 01（写字节个数） ff（要写的字节）
+func (self *PacketEnCode) WriteMultipleCoil(slaveAddr byte, startaddr uint16, coilNum uint16, value []byte) (error, []byte) {
+	var packet []byte
+	packet = append(packet, slaveAddr)
+	packet = append(packet, byte(FuncCodeWriteMultipleCoils))
+	packet = append(packet, uint16ToByte(startaddr)...)
+	packet = append(packet, uint16ToByte(coilNum)...)
+	packet = append(packet, byte(len(value)))
+	packet = append(packet, value...)
 	//加上crc
 	var crc crc
 	crc.reset()
